d3bd9e608d602e78db26b32672a7b775e33036d0
Fahad Kamal <kawsarkamal71@gmail.com>
2019-01-01 06:23:47 -0600

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -21,3 +21,23 @@ $(document).ready(function(){
 	});
 	
 });
+
+
+function myFunction() {
+  var input, filter, table, tr, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    a = tr[i].getElementsByTagName("a")[0];
+    if (a) {
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
